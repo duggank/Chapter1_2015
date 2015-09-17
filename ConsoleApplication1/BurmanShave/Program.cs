743fb8af204b4026c092ebb4f50a4f5649b412ba
duggank <CreativeWeep@gmail.com>
2015-09-17 11:04:16 -0400

    Commit #5
    Blooperdooperdoop

--- ConsoleApplication1/BurmanShave/Program.cs
+++ ConsoleApplication1/BurmanShave/Program.cs
@@ -9,7 +9,8 @@ namespace BurmanShave
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Driving cars\nReally Nice\nDangerous Roads\nDon't drive on ice...\nBurmaShave");
+            Console.ReadKey();
         }
     }
 }
